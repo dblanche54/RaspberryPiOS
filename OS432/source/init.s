f3ab8b207fd09ae710a9c1fd4d11a660c06a67f1
okz144 <okz144@USASK.CA>
2014-03-19 18:20:50 +0000

    Added new files missed in the last commit.

--- OS432/source/init.s
+++ OS432/source/init.s
@@ -8,12 +8,7 @@ _start:
 * Branch to the main function defined in main.c.
 */
 
-mov sp, #0x8000
-b main
-mov r0, #16
-mov r1, #1
-bl SetGpioFunction
-mov r0, #16
-mov r1, #0
-bl SetGpio
-b master_process
+mov sp, #0x8000			/* 0x8000 */
+b main					/* 0x8004 */
+b master_process		/* 0x8008 */
+mov r0, r0				/* 0x800C */
